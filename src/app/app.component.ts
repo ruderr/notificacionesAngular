e16bfc2c73053de240acdf3933853c1bb19c6d6b
ruderr <ruderr@gmail.com>
2020-06-12 00:26:45 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NotificationService } from './notification.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notificaciones';
+
+  constructor(private notifyService : NotificationService) { }
+
+  showToasterSuccess(){
+    this.notifyService.showSuccess("Data shown successfully !!", "Notificación")
+  }
+
+  showToasterError(){
+      this.notifyService.showError("Something is wrong", "Notificación")
+  }
+
+  showToasterInfo(){
+      this.notifyService.showInfo("This is info", "Notificación")
+  }
+
+  showToasterWarning(){
+      this.notifyService.showWarning("This is warning", "Notificación")
+  }
 }
